aef76a1be88dc967cce58420fe6b2bf532f3fd41
Charly Mourglia <charly.mourglia@gmail.com>
2021-12-11 01:01:33 +0100

    Solution for day03

--- src/day03.zig
+++ src/day03.zig
@@ -10,10 +10,78 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("../data/day03.txt");
+// const data = @embedFile("../data/day03-tst.txt");
 
 pub fn main() !void {
-    
+    var grid = try Grid.init();
+
+    // Part1
+    // const slopes = [1][2]usize{
+    //     [2]usize{ 3, 1 },
+    // };
+
+    const slopes = [5][2]usize{
+        [2]usize{ 1, 1 },
+        [2]usize{ 3, 1 },
+        [2]usize{ 5, 1 },
+        [2]usize{ 7, 1 },
+        [2]usize{ 1, 2 },
+    };
+
+    var mult: usize = 1;
+
+    for (slopes) |slope| {
+        var i: usize = 0;
+        var j: usize = 0;
+
+        var cpt: usize = 0;
+        while (j < grid.h) : ({
+            i += slope[0];
+            j += slope[1];
+        }) {
+            if (grid.hasTree(i, j)) {
+                cpt += 1;
+            }
+        }
+        mult *= cpt;
+    }
+
+    print("{}\n", .{mult});
 }
+
+const Grid = struct {
+    w: usize,
+    h: usize,
+    trees: List(bool),
+
+    fn init() !Grid {
+        var grid = Grid{
+            .w = 0,
+            .h = 0,
+            .trees = List(bool).init(gpa),
+        };
+
+        var it = tokenize(u8, data, "\r\n");
+        while (it.next()) |line| {
+            if (grid.w == 0) {
+                grid.w = line.len;
+            }
+            assert(grid.w == line.len);
+
+            grid.h += 1;
+
+            for (line) |c| {
+                try grid.trees.append(c == '#');
+            }
+        }
+
+        return grid;
+    }
+
+    fn hasTree(self: Grid, i: usize, j: usize) bool {
+        return self.trees.items[(i % self.w) + j * self.w];
+    }
+};
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
